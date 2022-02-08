0940ca6e4b33dd4db4dd8a422ce38130f3231a25
Nishanth <rajkumarnishanth@gmail.com>
2022-02-08 20:36:16 +0530

    Added functionality to find length of line

--- GeometryLine/Program.cs
+++ GeometryLine/Program.cs
@@ -1,3 +1,15 @@
-﻿Console.Title = "Line Comparison App";
+﻿using GeometryLine;
+
+Console.Title = "Line Comparison App";
 Console.WriteLine("----------------------Geometry Line Comparison App----------------------");
+Line line1 = new Line();
+Line Line2 = new Line();
+Console.WriteLine("Enter line 1 details: ");
+line1.GetPointsFromUser();
+Console.WriteLine("Enter line 2 details: ");
+Line2.GetPointsFromUser();
+Console.WriteLine("\nLine 1: ");
+line1.Describe();
+Console.WriteLine("Line 2: ");
+Line2.Describe();
 Console.ReadKey();
